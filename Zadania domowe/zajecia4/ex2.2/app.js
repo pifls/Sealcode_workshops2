cb62cc09842eff5aaed00399c07a0423538a56ea
pifls <marek.pikosz@gmail.com>
2018-03-26 15:13:03 +0200

    added ex2.2

---
+++ Zadania domowe/zajecia4/ex2.2/app.js
@@ -0,0 +1,48 @@
+function main() {
+  console.log("Rozpoczynam zadanie...");
+    let a = 5;
+    let b = 0;
+    const result = divide(a, b);
+    let x = 7;
+    console.time("Czas wykonywania pętli for");
+    for (let i = 0; i < 3; i++) {
+        a++;
+        b--;
+        console.log(i);
+        x++;
+    }
+    console.timeEnd("Czas wykonywania pętli for");
+    x = x - 7;
+    if(x < 5){
+      console.warn("Wartosc x jest mniejsze od 5");
+    }
+    let tab = [];
+    if(tab.length == 0){
+      console.info("Tablica tab jest pusta");
+    }
+    let tab2 = [
+        {name: "Nazwa 1", color: "red", howMuch: 5},
+        {name: "Nazwa 2", color: "green", howMuch: 1},
+        {name: "Nazwa 3", color: "orange", howMuch: 3},
+        {name: "Nazwa 4", color: "blue", howMuch: 7}
+    ];
+    console.table(tab2);
+
+    debugowanie();
+}
+
+function divide(x, y) {
+  if(y == 0){
+    console.error("Dzielenie przez 0!");
+  }
+    return x/y;
+}
+
+function debugowanie() {
+    console.log("Debugowanie");
+    const y = 5;
+
+    console.assert(y>3, "Błąd zmiennej y!");
+}
+
+window.onload = main;
